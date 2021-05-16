9b3e609671124b3d1d73342aa590b5f9852de066
Rachel Amos <rachelamos35@gmail.com>
2021-05-16 15:55:05 -0500

    deleted some React files, updated index and app js files, added dependencies, and added API js file

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [],
+      loading: false
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((response) => {
+      this.setState({
+        items:response.results,
+        loading:true
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Hello world</div>
   );
 }
 
